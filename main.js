b6652f41d2cbe404927148455b953f6dd4169149
Matěj <xkonop03@vutbr.cz>
2022-12-12 02:57:15 +0100

    added functionality on cooking page

--- main.js
+++ main.js
@@ -131,10 +131,35 @@ async function getIngredientsByRecipeId(id) {
     return getRecipeById(id).ingredients;
 }
 
-function generateIngredientDiv(name) {
+function generatePortionPicker(){
+    return `
+    <div class="portion-picker-container">
+        <input type="range" class="form-range gray-100" min="0" max="10" step="1" id="customRange3" oninput="updatePortionCount()">
+        <div class="portion-count" id="portionCount">Počet porcí: 5</div>
+        <button type="button" class="btn btn-primary" id="portionButton" onclick="setPortionCount()">Vařit!</button>
+    </div>
+    `
+}
+
+function setPortionCount() {
+    let range = document.getElementById("customRange3");
+    let portionCount =  range.value;
+
+    range.style.display = "none";
+    document.getElementById("portionButton").style.display = "none";
+
+    let ingredientItems = document.getElementsByClassName("ingredient-item");
+
+}
+function updatePortionCount() {
+    let portionCount = document.getElementById("customRange3").value;
+    document.getElementById("portionCount").innerHTML = "Počet porcí: " +  portionCount;
+}
+
+function generateIngredientDiv(name, amount, unit) {
     return `        
             <div class="ingredient-item">
-                <label class="ingredient-radio-container">${name}
+                <label class="ingredient-radio-container">${name} 
                   <input type="checkbox">
                   <span class="ingredient-radio-checkmark"></span>
                 </label>
@@ -142,34 +167,59 @@ function generateIngredientDiv(name) {
     `
 }
 
+function setStepAsDone(step, id) {
+    let nextID = parseInt(id) + 1;
+    console.log(step + " CHANGED!");
+    let stepToChange = document.getElementById(step);
+    console.log(stepToChange);
+    if (stepToChange.className == "step-description-hidden") {
+        stepToChange.className = "step-description-visible";
+    } else {
+        stepToChange.className = "step-description-hidden";
+        console.log("Scrolling to " + "step_" + nextID);
+        document.getElementById("step_" + nextID ).scrollIntoView();
+    }
+}
+
 function generateStepDiv(id, name, description, time) {
-    const step_id = "step_" + id;
+    const stepID = "step_" + id;
+    const timeline = document.getElementById("timeline");
     let htmlSegment = `
-            <div id=${step_id} class="step">
+            <div id=${stepID} class="step">
+                <label class="ingredient-radio-container step-radio-container">
+                  <input type="checkbox" onchange="setStepAsDone('${stepID+"_p"}', '${id}')">
+                  <span class="ingredient-radio-checkmark step-radio-checkmark"></span>
+                </label>
+                <div class="step-content">
+                <div class="step-header">  
                 <h2 class="step_name">${name}</h2>
-                <div class="step-contents">
-                <p class="step_description">${description}</p>`;
+                `;
 
     //set up timer, if the step has a valid duration value for timing
-    if (time === -1) {
+    if (time > 0) {
         htmlSegment += `
-             </div>
-             </div>
-        `
-    } else {
-        htmlSegment +=`
-            <svg class="timer-icon" onclick="getNewTimer(${id}, ${time}, '${name}')" width="3em" height="3em"  version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 296.228 296.228" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 296.228 296.228">
+            <div class="timer-wrap" onclick="getNewTimer(${id}, ${time}, '${name}')">
+            <p class="timer-text">${time}m</p>
+            <svg class="timer-icon"  width="3em" height="3em"  version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 296.228 296.228" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 296.228 296.228">
               <g>
                 <path d="m167.364,48.003v-23.003h10.5c6.903,0 12.5-5.597 12.5-12.5s-5.596-12.5-12.5-12.5h-59.5c-6.903,0-12.5,5.597-12.5,12.5s5.597,12.5 12.5,12.5h10.5v23.003c-59.738,9.285-105.604,61.071-105.604,123.37-3.55271e-15,68.845 56.01,124.854 124.854,124.854s124.854-56.01 124.854-124.854c0-62.299-45.866-114.085-105.604-123.37zm-19.25,223.225c-55.06,0-99.854-44.795-99.854-99.854s44.795-99.854 99.854-99.854 99.854,44.795 99.854,99.854-44.794,99.854-99.854,99.854z"/>
                 <path d="m160.614,166.18v-58.889c0-6.903-5.597-12.5-12.5-12.5s-12.5,5.597-12.5,12.5v66.1c0,2.033 0.81,3.982 2.25,5.416l34.969,34.822c4.893,4.872 12.806,4.854 17.678-0.037 4.871-4.892 4.854-12.807-0.037-17.678l-29.86-29.734z"/>
               </g>
             </svg>
-            </div>
-            </div>`
+            </div>`;
     }
+
+    htmlSegment += `
+            </div>
+            <p id="${stepID+"_p"}" class="step-description-visible">${description}</p>
+            </div>
+            </div>`;
+
+
+
+    //create timeline
     return htmlSegment;
 }
-
 
 
 async function renderRecipe(recipeId) {
@@ -177,28 +227,35 @@ async function renderRecipe(recipeId) {
 
     const recipe = recipes.find(({id}) => id == recipeId);
 
-    let htmlResponse = `<div class="recipe-detail"><h1>${recipe.name}</h1>
-                        <h2 class="ingredient-header">Nachystej si ingredience</h2>
+    let htmlResponse = `<div class="recipe-detail"><h1>${recipe.name}</h1>`;
+
+    //render portion counter
+    htmlResponse += generatePortionPicker();
+
+    htmlResponse += `   <h2 class="ingredient-header">Nachystej si ingredience</h2>
                         <div class="ingredient-container">`;
     //render ingredients
     recipe.ingredients.forEach((ingredient) => {
         htmlResponse += generateIngredientDiv(ingredient);
     })
 
+    //render recipes
+
     htmlResponse += `
                 </div>
-                <h2 class="steps-header">Jde se vařit</h2>`;
+                <h2 class="steps-header">Jde se vařit</h2>
+                <div class="steps-container">
+                `;
     let i = 0;
     recipe.steps.forEach((step) => {
         console.log("STEP:" + step);
         htmlResponse += generateStepDiv(i, step.name, step.description, step.time);
         i++;
     });
-    htmlResponse += `<div class="timer_container" id="timer_container"></div></div>`;
+    htmlResponse += `<div class="timer_container" id="timer_container"></div></div></div>`;
     console.log(htmlResponse);
     return htmlResponse;
 }
-
 
 
 
@@ -212,11 +269,11 @@ function getNewTimer(id, minutes, name) {
     const newTimer = document.createElement("div");
     newTimer.classList.add("timer");
     newTimer.id = "timer_" + id;
-    newTimer.textContent = "Timer: " + name;
+    newTimer.textContent = "Časovač: " + name;
 
 
     const quitButton = document.createElement("button");
-    quitButton.textContent = "End";
+    quitButton.textContent = "Ukončit";
     quitButton.addEventListener('click', () => {
         quitButton.parentElement.parentElement.removeChild(newTimer);
         clearTimeout(downloadTimer)
