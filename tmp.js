646320e32121a564ac0f19bd02730a6f0ff27205
Martin Pech <39264129+martafonekVOLE@users.noreply.github.com>
2022-12-12 19:43:12 +0100

    working edits

--- tmp.js
+++ tmp.js
@@ -1,57 +1,77 @@
-function checkDesc(){   
-    if(window.localStorage.getItem("currDesc") == null){
-        var currDesc = 1;
-        var currIngredient = null;
-    }
-    else{
-        var currDesc = window.localStorage.getItem("currDesc");
-        currDesc = parseInt(currDesc);
-        if(currDesc == 1){
-            var currIngredient = document.getElementById("desc"+(currDesc)+"1");
+function checkingredients(){   
+    var tokenGo = false;
+    if(window.localStorage.getItem("currNumber") == null){
+        if(window.localStorage.getItem("currNumber2") != null){
+            currNumber = window.localStorage.getItem("currNumber2");
+            window.localStorage.removeItem("currNumber2");
+            console.log(currNumber);
+            tokenGo = true;
         }
         else{
-            var currIngredient = document.getElementById("desc"+(currDesc-1)+"1");
+            var currNumber = 1;
+            var currIngredient = null;
         }
-    }
-    if(currDesc == 1){
-        var content = document.getElementById("auto-fill-content"+(currDesc));
     }
     else{
-        var content = document.getElementById("auto-fill-content"+(currDesc-1));
-    }
-
-    if(content.innerHTML == null || content.innerHTML.trim() == ""){
-        var newField = `<h3 id="${currDesc}"></h3>`; 
-        newField += `<input type="text" class="col-sm-5" placeholder="Jméno ingredience" required name="desc${currDesc}1" id="desc${currDesc}1">`;
-        newField += `<input type="number" class="col-sm-3" placeholder="Množství ingredience" required name="desc${currDesc}2" id="desc${currDesc}2">`;
-        newField += `<input type="text" class="col-sm-3" placeholder="Jednotka" required name="desc${currDesc}3" id="desc${currDesc}3">`; //TODO replace dropdown
-        newField += `<span class="col-sm-1" onclick="removeIngredient(${currDesc})">&times;<span>`;
-        newField += `</div>`;
-        content.innerHTML = newField;
-        window.localStorage.removeItem("currDesc");
-        var nextNumber = currDesc+1;
-        window.localStorage.setItem("currDesc", nextNumber);
-        console.log(window.localStorage.getItem("currDesc"));
-        if(currDesc != 1){    
-            setInterval(document.getElementById("desc"+(currDesc-1)+"1").focus(), 100);
+        var currNumber = window.localStorage.getItem("currNumber");
+        currNumber = parseInt(currNumber);
+        if(currNumber == 1 || tokenGo){
+            var currIngredient = document.getElementById("ingredients"+(currNumber)+"1");
+        }
+        else{
+            var currIngredient = document.getElementById("ingredients"+(currNumber-1)+"1");
         }
     }
+    if(currNumber == 1 || tokenGo){
+        var content = document.getElementById("auto-fill-ingredients"+(currNumber));
+    }
+    else{
+        var content = document.getElementById("auto-fill-ingredients"+(currNumber-1));
+    }
+
+    if(content != null){
+        if(content.innerHTML == null || content.innerHTML.trim() == ""){
+            var newField = `<h3 id="${currNumber}"></h3>`; 
+            newField += `<input type="text" class="col-sm-5" placeholder="Název kroku" required name="ingredients${currNumber}1" id="ingredients${currNumber}1">`;
+            newField += `<input type="text" class="col-sm-3" placeholder="Popis" required name="ingredients${currNumber}2" id="ingredients${currNumber}2">`;
+            newField += `<input type="text" class="col-sm-3" placeholder="Čas provádění (v minutách)" name="ingredients${currNumber}3" id="ingredients${currNumber}3">`; //TODO replace dropdown
+            newField += `<span class="col-sm-1" onclick="removeIngredient(${currNumber})">&times;</span>`;
+            newField += `</div>`;
+            content.innerHTML = newField;
+            if(tokenGo){
+                window.localStorage.removeItem("currNumber2");
+                tokenGo = false;
+                var nextNumber = parseInt(currNumber)+1;
+            }
+            else{
+                window.localStorage.removeItem("currNumber");
+                var nextNumber = (currNumber)+1;
+            }
+            window.localStorage.setItem("currNumber", nextNumber);
+            console.log('here:');
+            console.log(window.localStorage.getItem("currNumber"));
+            if(currNumber != 1 && tokenGo){    
+                setInterval(document.getElementById("ingredients"+(currNumber-1)+"1").focus(), 100);
+            }
+        }
+    }
+
 
     if(currIngredient !== document.activeElement){
         return;
     }
     else{
-        var newField = `<div class="mb-3" id="auto-fill-content${currDesc}"><h3 id="${currDesc}"></h3>`; 
-        newField += `<input type="text" class="col-sm-5" placeholder="Jméno ingredience" name="desc${currDesc}1" id="desc${currDesc}1">`;
-        newField += `<input type="number" class="col-sm-3" placeholder="Množství" name="desc${currDesc}2" id="desc${currDesc}2">`;
-        newField += `<input type="text" class="col-sm-3" placeholder="Jednotka" name="desc${currDesc}3" id="desc${currDesc}3">`;
+        var newField = `<div class="mb-3" id="auto-fill-ingredients${currNumber}"><h3 id="${currNumber}"></h3>`; 
+        newField += `<input type="text" class="col-sm-5" placeholder="Název kroku" name="ingredients${currNumber}1" id="ingredients${currNumber}1">`;
+        newField += `<input type="text" class="col-sm-3" placeholder="Popis" name="ingredients${currNumber}2" id="ingredients${currNumber}2">`;
+        newField += `<input type="text" class="col-sm-3" placeholder="Čas provádění (v minutách)" name="ingredients${currNumber}3" id="ingredients${currNumber}3">`;
          //TODO replace dropdown
-        newField += `<span class="col-sm-1" onclick="removeIngredient(${currDesc})">&times;<span></div>`;
+        newField += `<span class="col-sm-1" onclick="removeingredients(${currNumber})">&times;</span></div>`;
         content.innerHTML += newField;
-        window.localStorage.removeItem("currDesc");
-        var nextNumber = currDesc+1;
-        window.localStorage.setItem("currDesc", nextNumber);
-        console.log(window.localStorage.getItem("currDesc"));
-        setInterval(document.getElementById("desc"+(currDesc-1)+"1").focus(), 100);
+        window.localStorage.removeItem("currNumber");
+        var nextNumber = currNumber+1;
+        window.localStorage.setItem("currNumber", nextNumber);
+        console.log(window.localStorage.getItem("currNumber"));
+        setInterval(document.getElementById("ingredients"+(currNumber-1)+"1").focus(), 100);
     }
 }
